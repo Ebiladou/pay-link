dd3d73914ea0469a7051a2b22b13d17d1350c1a0
Ebiladou <oyinebiladouomemu@gmail.com>
2026-03-14 13:20:01 +0100

    duplicate notification fix

--- app/services/notification.py
+++ app/services/notification.py
@@ -2,24 +2,25 @@ from app.core.logger import logger
 from app.core.database import SessionDep
 from app.core.model import Users, Transactions
 from app.core.model import Notifications
+from sqlmodel import select
 
 class NotificationService:
-    async def transaction_success(
-        self,
-        session: SessionDep,
-        user: Users,
-        transaction: Transactions,
-    ):
+    async def transaction_success(self, session: SessionDep, user: Users, transaction: Transactions,):
 
         message = f"You received {transaction.amount} NGN from {transaction.email} for reference {transaction.reference}."
 
-        notification = Notifications(
-            user_id=user.id,
-            message=message
-        )
+        result = await session.exec(select(Notifications).where(Notifications.user_id == user.id, Notifications.transaction_id == transaction.id))
+        existing_notification = result.first()
 
-        session.add(notification)
-        await session.commit()
+        if existing_notification is None:
+            notification = Notifications(
+                user_id=user.id,
+                transaction_id=transaction.id,
+                message=message
+            )
+
+            session.add(notification)
+            await session.commit()
 
         return True
 
